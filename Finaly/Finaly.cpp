1e79d60ca74455130198b198e26039b70f643bfa
beigai666 <739771064@qq.com>
2021-12-28 22:50:18 +0800

    kmp上

--- Finaly/Finaly.cpp
+++ Finaly/Finaly.cpp
@@ -7,6 +7,53 @@
 #include <String/String.h>
 using namespace std;
 using namespace FinlayLib;
+int* make_pmt(const char* p) {
+	int len = strlen(p);
+	int* ret = static_cast<int*>(malloc(sizeof(int) * len));
+	if (ret != NULL)
+	{
+		int ll = 0;
+		ret[0] = 0;
+		for (int i = 1; i < len; i++)
+		{
+			while ((ll>0)&&(p[ll]!=p[i]))
+			{
+				ll = ret[ll - 1];
+			}
+			if (p[ll] == p[i]) {
+				ll++;
+			}
+			ret[i] = ll;
+		}
+	}
+	return ret;
+}
+
+int kmp(const char* s, const char* p)
+{
+	int ret = -1;
+	int sl = strlen(s);
+	int pl = strlen(p);
+	int* pmt = make_pmt(p);
+	if ((pmt != NULL) && (0 < pl) && (pl <= sl)) {
+		for (int i = 0,j=0; i < sl; i++)
+		{
+			while ((j>0)&&(s[i]!=p[j]))
+			{
+				j = pmt[j - 1];
+			}
+			if (s[i] == p[j]) {
+				j++;
+			}
+			if (j == pl)
+			{
+				ret = i + 1 - pl;
+			}
+		}
+	}
+	free(pmt);
+	return ret;
+}
 
 void test_1() {
 	cout << "test_1()begin ..." << endl;
@@ -42,10 +89,18 @@ void test_2() {
 // TODO: 这是一个库函数示例
 void fnFinaly()
 {
+	/*
 	String s = "   abc   ";
 	if (s.trim().insert(0, "D.T.").endOf("abc") && s.startWith("D.T."))
 	{
 		cout <<"["<< s.str()<<"]" << endl;
 	}
 	cout << s.str() << endl;
+	*/
+	int* pmt = make_pmt("ABCDABD");
+	for (int i = 0; i < strlen("ABCDABD"); i++) {
+		cout << i << " : " << pmt[i]<<endl;
+	}
+
+	cout << kmp("abcdcdcdcde", "cde") << endl;
 }
